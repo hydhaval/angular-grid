9a94e37bb7e38c85ff401a315147bd34ada1f554
Dhaval Patel <dhaval_patel@apple.com>
2017-05-14 00:05:24 -0700

    unit test begins

--- test/spec/exampleCtrl.test.js
+++ test/spec/exampleCtrl.test.js
@@ -13,6 +13,17 @@ describe('calculator', function() {
   }));
 
   describe('scope function validation', function() {
+    it('it should perform defalut assigning validation', function() {
+      var $scope = {};
+      var controller = $controller('ExampleCtrl', {
+        $scope: $scope
+      });
+      expect($scope.CurrentPage).toBe(1);
+      expect($scope.TotalPage).toBe(10);
+      expect($scope.PerPage).toBe(10);
+      expect($scope.recordData.length).toBe(0);
+    });
+
     it('it should validate GetRecordData function', function() {
       var $scope = {};
       var controller = $controller('ExampleCtrl', {
